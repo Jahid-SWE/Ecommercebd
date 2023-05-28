7d516fe93277e753d5ae32721c2c11a69d800d03
MD. Jahid Hassan <33800850+Jahid-SWE@users.noreply.github.com>
2023-05-28 22:24:18 +0600

    new method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,11 @@ class HomeController extends Controller
     public function index(){
         return view('home.userpage');
     }
+
+    public function create(){
+        return 'hello world';
+    }
+
     public function redirect(){
         $usertype=Auth::user()->usertype;
         if($usertype=='1'){
