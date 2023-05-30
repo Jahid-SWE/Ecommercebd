6ccaf67b1d20c4bee1dd9e5407f7d72e7a3b8e3b
MD. Jahid Hassan <33800850+Jahid-SWE@users.noreply.github.com>
2023-05-30 23:12:48 +0600

    new route and controller crete

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,11 +11,6 @@ class HomeController extends Controller
     public function index(){
         return view('home.userpage');
     }
-
-    public function create(){
-        return 'hello world';
-    }
-
     public function redirect(){
         $usertype=Auth::user()->usertype;
         if($usertype=='1'){
