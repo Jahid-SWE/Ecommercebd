ec7fd8a66f15ea8fb622f399acd9f06f212e22e6
MD. Jahid Hassan <33800850+Jahid-SWE@users.noreply.github.com>
2023-05-31 22:23:57 +0600

    method declear with reture view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,9 @@ class HomeController extends Controller
     public function index(){
         return view('home.userpage');
     }
+    public function contact(){
+        return view('home.contact');
+    }
     public function redirect(){
         $usertype=Auth::user()->usertype;
         if($usertype=='1'){
